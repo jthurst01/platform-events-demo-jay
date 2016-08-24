ff477240a73ec0684d98b312e2475b5613e46a44
Jay Hurst <jhurst@salesforce.com>
2016-08-24 03:10:29 -0700

    Initial Push - 5

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Force.com Streaming API Demo with nforce' });
 });
 
+router.get('/default', function(req, res) {
+  res.render('default', { title: 'Force.com Streaming API Demo with nforce' });
+});
+
 module.exports = router;
